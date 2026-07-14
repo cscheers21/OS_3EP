a029a227038d7106cf391be83b0733d0308315c5
c_scheers <cscheers@ualberta.ca>
2026-07-13 19:01:15 -0600

    base wcat

---
+++ initial-utilities/wcat/wcat.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        return 0;
+    }
+    
+    FILE *fp = fopen(argv[1], "r");
+    if (fp == NULL) {
+        printf("cannot open file\n");
+        exit(1);
+    }
+
+    char buffer[100];
+
+    while (fgets(buffer, sizeof(buffer), fp)) {
+        printf("%s", buffer);
+    }
+    fclose(fp);
+
+    return 0;
+}
