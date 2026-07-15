8c5e15d101e892ab42d298590270320f2c1006c0
c_scheers <cscheers@ualberta.ca>
2026-07-15 17:09:56 -0600

    modify wzip for multiple files

--- initial-utilities/wzip/wzip.c
+++ initial-utilities/wzip/wzip.c
@@ -1,38 +1,46 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
+
     if (argc < 2) {
         printf("wzip: file1 [file2 ...]\n");
         return 1;
     }
 
-    FILE *fp = fopen(argv[1], "r");
-    if (fp == NULL) {
-        printf("Could not open file\n");
-        return 1;
-    }
-
-    int prev_char = fgetc(fp);
-    if (prev_char == EOF) {
-        return 0;
-    }
-
-    int counter = 1;
+    int index = 1;
+    int prev_char;
     int sent_char;
+    int counter = 1;
+    FILE *fp = NULL;
 
-    while (prev_char != EOF) {
-        sent_char = fgetc(fp);
-        if (sent_char != prev_char) {
-            fwrite(&counter, sizeof(int), 1, stdout);
-            fwrite(&prev_char, sizeof(char), 1, stdout);
-            counter = 1;
-        }
-        else {
-            counter++;
+    while (index < argc) {
+        fp = fopen(argv[index], "r");
+        if (fp == NULL) {
+            printf("Could not open file\n");
+            return 1;
         }
 
-        prev_char = sent_char;
+        prev_char = fgetc(fp);
+        if (prev_char == EOF) {
+            index++;
+            continue;
+        }
+
+        while (prev_char != EOF) {
+            sent_char = fgetc(fp);
+            if (sent_char != prev_char) {
+                fwrite(&counter, sizeof(int), 1, stdout);
+                fwrite(&prev_char, sizeof(char), 1, stdout);
+                counter = 1;
+            }
+            else {
+                counter++;
+            }
+
+            prev_char = sent_char;
+        }
+        fclose(fp);
+        index++;
     }
-
     return 0;
 }
